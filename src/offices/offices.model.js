c2b2b3f278c4f7c12105ac6859de23882c6972ff
Fabian Mendoza <yessid.mendoza@gmail.com>
2017-11-17 00:33:18 -0500

    I have more questions than solutions here :fearful: :sob:

--- src/offices/offices.model.js
+++ src/offices/offices.model.js
@@ -4,3 +4,11 @@ const Office = {};
 Office.create = function(office){
     return knex('offices').insert(office);    
 };
+Office.find = function(office){
+    return knex.select(office).from('offices');
+};
+Office.updateByID = function(office){
+    return knex('offices').where('identifier','=', office.identifier).update({});
+}
+
+
